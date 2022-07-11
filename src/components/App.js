0d08c789914d3326155c3b2aa35d3c0c8520755b
Kane Mathis <admin@kanemathis.com>
2022-07-11 12:52:55 -0400

    cards successfully displayed

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,6 +6,15 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [data, setData] = useState([])
+
+  useEffect(()=>
+  {
+    fetch('http://localhost:3001/toys')
+    .then(res=>res.json())
+    .then(data=>setData(data))
+  },[])
+
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
@@ -18,7 +27,7 @@ function App() {
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer data={data}/>
     </>
   );
 }
--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({data}) {
+
+  console.log('data in toyContainer', data)
+  
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{
+      data.map((index)=>{return <ToyCard id={index.id} data={index}/>})
+
+    }</div>
   );
 }
 
